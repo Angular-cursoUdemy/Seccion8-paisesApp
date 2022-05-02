dd0cb86aaf5bd7a152ad7cdea4db3a502f000405
jcalvolo <jcalvolo@emeal.nttdata.com>
2022-05-02 18:31:12 +0200

    primera subida

---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/pais.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  constructor(private http: HttpClient) { }
+
+  buscarPais ( dato: string): Observable<Country[]>{
+
+  const url = `${ this.apiUrl }/name/${ dato }`;
+
+  return this.http.get<Country[]>(url);
+
+  }
+  buscarCapital ( dato: string): Observable<Country[]>{
+
+    const url = `${ this.apiUrl }/capital/${ dato }`;
+    return this.http.get<Country[]>(url);
+  }
+  getPaisPorAlpha ( dato: string): Observable<Country[]>{
+
+    const url = `${ this.apiUrl }/alpha/${ dato }`;
+    return this.http.get<Country[]>(url);
+  }
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+
+    `li
+    {cursor: pointer;}
+    `
+  ]
+  //Pa la manita 
+})
+export class SidebarComponent  {
+
+
+
+  constructor() { }
+
+  
+
+}
